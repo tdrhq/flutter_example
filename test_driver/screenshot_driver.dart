44f6184d1183e5dc1c292c9ac1a001795cf54106
Arnold Noronha <arnold@tdrhq.com>
2022-10-27 15:20:11 -0400

    Rename driver

--- test_driver/screenshot_driver.dart
+++ test_driver/screenshot_driver.dart
@@ -1,4 +1,4 @@
-//test_driver/foo_test.dart
+//test_driver/screenshot_driver.dart
 import 'dart:io';
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:integration_test/integration_test_driver_extended.dart';
